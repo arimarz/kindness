5a3e3f8273b1be4d85d5fb50343c2d6373ee0dbe
Bianca Aspin <bianca@mj94.net>
2024-01-24 02:22:03 -0800

    Create initial component with placeholder elements.

---
+++ my-app/components/DayView.js
@@ -0,0 +1,13 @@
+import { StyleSheet, View, Text, Pressable } from 'react-native';
+
+function DayView({date, act}){
+    return (
+        <View>
+            <Text>Date</Text>
+            <Text>Act</Text>
+        </View>
+    )
+
+}
+
+export default DayView
